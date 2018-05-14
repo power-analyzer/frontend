0e563e31203c1631fa05c9e837598e8030935757
Ryan Rabello <ryan.s.rabello@gmail.com>
2018-05-13 18:40:14 -0700

    Nav-bar now written in Reactstrap

--- src/navbar.js
+++ src/navbar.js
@@ -3,36 +3,48 @@ import { Link } from 'react-router-dom';
 import {
   Navbar,
   Nav,
-  NavItem
-
-} from 'react-bootstrap';
+  NavItem,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+} from 'reactstrap';
 
 export class MainNav extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   render() {
     return (
-      <Navbar inverse collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to="dashboard">Power Analyzer</Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav>
-            <NavItem>
-              <Link to="dashboard">Dashboard</Link>
-            </NavItem>
-            <NavItem>
-              <Link to="data">Data</Link>
-            </NavItem>
-          </Nav>
-          <Nav pullRight>
-            <NavItem>
-              <Link to="/">Logout</Link>
-            </NavItem>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <Navbar color="dark" dark expand="md">
+          <NavbarBrand><Link className="navbar-brand" to="dashboard">Power Analyzer</Link></NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <Link className="nav-link" to="dashboard">Dashboard</Link>
+              </NavItem>
+              <NavItem>
+                <Link className="nav-link" to="data">Data</Link>
+              </NavItem>
+            </Nav>
+            <Nav navbar>
+              <NavItem>
+                <Link className="nav-link" to="/">Logout</Link>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
     );
   }
 }
