f0f2e432df50a80098e78eb0c59ee845144ce3a9
Ryan Rabello <ryan.s.rabello@gmail.com>
2018-05-24 14:09:40 -0700

    Login page

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Container,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Row,
+  Col,
+} from 'reactstrap';
+
+import logo from '../media/logo.png';
 
 export class LoginRoute extends Component {
   render() {
     return(
-      <h1>Login</h1>
+      <Container>
+        <Row>
+          <Col xs="12" md={{"size": 6, "offset": 3}} lg={{"size": 4, "offset": 4}} className="text-center mt-5">
+            <img src={logo} alt="Power Analyzer Logo" className="img-fluid"/>
+            <Form className="mt-3">
+              <FormGroup>
+                <Input type="text" name="username" id="username" placeholder="Username" />
+              </FormGroup>
+              <FormGroup>
+                <Input type="password" name="password" id="examplePassword" placeholder="Password" />
+              </FormGroup>
+              <Link to="dashboard" className="btn btn-warning">Login</Link>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
